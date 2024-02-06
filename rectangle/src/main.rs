d0de2173082dac33e4965a495b87619fc49a024d
Moises Rodriguez <moisesrj97@gmail.com>
2024-02-06 20:28:32 +0100

    refactor: use tuple

--- rectangle/src/main.rs
+++ rectangle/src/main.rs
@@ -1,12 +1,13 @@
 fn main() {
     let height: i64 = 10;
     let width: i64 = 20;
+    let dimensions = (height, width);
 
-    let area: i64 = area(height, width);
+    let area: i64 = area(dimensions);
 
     println!("The area is {area}")
 }
 
-fn area(height: i64, width: i64) -> i64 {
-    return height * width;
+fn area(dimensions: (i64, i64)) -> i64 {
+    return dimensions.0 * dimensions.1;
 }
