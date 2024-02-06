350330d5100daf8e4a17818690825c219f33b346
Moises Rodriguez <moisesrj97@gmail.com>
2024-02-06 20:35:08 +0100

    refactor: move function to struct method

--- rectangle/src/main.rs
+++ rectangle/src/main.rs
@@ -3,16 +3,18 @@ struct Rectangle {
     width: i64,
 }
 
+impl Rectangle {
+    fn area(&self) -> i64 {
+        return self.width * self.height;
+    }
+}
+
 fn main() {
     let height: i64 = 10;
     let width: i64 = 20;
     let rectangle = Rectangle { height, width };
 
-    let area: i64 = area(rectangle);
+    let area: i64 = rectangle.area();
 
     println!("The area is {area}")
-}
-
-fn area(rectangle: Rectangle) -> i64 {
-    return rectangle.width * rectangle.height;
 }
