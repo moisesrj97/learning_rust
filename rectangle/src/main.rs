1e51169b85ce0f1f295ececcf549a87ed1a7a13b
Moises Rodriguez <moisesrj97@gmail.com>
2024-02-06 20:40:24 +0100

    feat: implement can_hold function

--- rectangle/src/main.rs
+++ rectangle/src/main.rs
@@ -7,14 +7,28 @@ impl Rectangle {
     fn area(&self) -> i64 {
         return self.width * self.height;
     }
+    fn can_hold(&self, other_rectangle: &Rectangle) -> bool {
+        let is_wider = self.width >= other_rectangle.width;
+        let is_higher = self.height >= other_rectangle.height;
+        return is_higher && is_wider;
+    }
 }
 
 fn main() {
-    let height: i64 = 10;
-    let width: i64 = 20;
-    let rectangle = Rectangle { height, width };
+    let big_rectangle = Rectangle {
+        height: 10,
+        width: 10,
+    };
+    let small_rectangle = Rectangle {
+        height: 10,
+        width: 10,
+    };
 
-    let area: i64 = rectangle.area();
+    println!("Area of big rectangle is {}", big_rectangle.area());
+    println!("Area of small rectangle is {}", small_rectangle.area());
 
-    println!("The area is {area}")
+    print!(
+        "Can big rectangle contain small rectangle? {}",
+        big_rectangle.can_hold(&small_rectangle)
+    );
 }
