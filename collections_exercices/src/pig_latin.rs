2b2f584fc721e05fc0eef0d07f61502e748a4441
moisesrj97 <moises.rodriguez@codurance.com>
2024-02-12 14:18:09 +0100

    feat: add pig_latin function

--- collections_exercices/src/pig_latin.rs
+++ collections_exercices/src/pig_latin.rs
@@ -3,7 +3,28 @@
 // (“apple” becomes “apple-hay”). Keep in mind the details about UTF-8 encoding!
 
 fn pig_latin(input: String) -> String {
-    return String::from("irst-fay");
+    const VOWELS: [char; 5] = ['a', 'e', 'i', 'o', 'u'];
+
+    let mut result = Vec::new();
+
+    for word in input.split_whitespace() {
+        let first_letter = word.chars().nth(0).expect("The input is empty");
+        let is_first_letter_vowel = VOWELS.iter().any(|v| v.eq_ignore_ascii_case(&first_letter));
+
+        let mut transformed_word = String::from(word);
+
+        match is_first_letter_vowel {
+            false => {
+                transformed_word.remove(0);
+                transformed_word = transformed_word + "-" + &first_letter.to_string() + "ay";
+            }
+            true => transformed_word += "-hay",
+        }
+
+        result.push(transformed_word)
+    }
+
+    return result.join(" ");
 }
 
 #[cfg(test)]
@@ -11,12 +32,32 @@ mod tests {
     use super::*;
 
     #[test]
-    fn pig_latin_basic() {
+    fn pig_latin_vowel() {
         let input = String::from("first");
 
         let response = pig_latin(input);
 
         let expected_response = String::from("irst-fay");
+        assert_eq!(response, expected_response)
+    }
+
+    #[test]
+    fn pig_latin_consonant() {
+        let input = String::from("apple");
+
+        let response = pig_latin(input);
+
+        let expected_response = String::from("apple-hay");
+        assert_eq!(response, expected_response)
+    }
+
+    #[test]
+    fn pig_latin_sentence() {
+        let input = String::from("the apple is rotten");
+
+        let response = pig_latin(input);
+
+        let expected_response = String::from("he-tay apple-hay is-hay otten-ray");
         assert_eq!(response, expected_response)
     }
 }
